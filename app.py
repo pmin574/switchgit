fae9b45b055b85ee1bc3216ea9e92fc859c4ec42
pminne574 <pminne574@gmail.com>
2025-03-01 15:51:13 -0500

    added env variables

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import os
 import subprocess
 
 def run_command(command):
@@ -11,45 +11,46 @@ def run_command(command):
         return None
 
 def get_current_git_user():
-    # Get current global Git username and email
+   
     current_user = run_command('git config --global user.name')
     current_email = run_command('git config --global user.email')
     return current_user, current_email
 
 def update_git_credentials(new_user, new_email, account_type):
-    # Update global Git username and email
+    
     run_command(f'git config --global user.name "{new_user}"')
     run_command(f'git config --global user.email "{new_email}"')
 
-    # Notify the user via macOS notification
+    
     message = f"Switched to {account_type} account: {new_user} with email: {new_email}"
     print(message)
     send_notification("Git Account Switch", message)
 
 def send_notification(title, message):
-    # Use AppleScript via osascript to trigger a macOS notification
+
     script = f'display notification "{message}" with title "{title}"'
     subprocess.run(["osascript", "-e", script])
 
 def switch_git_user():
-    # Define your personal and school credentials
-    personal_user = "pminne574"
-    personal_email = "pminne574@gmail.com"
 
-    school_user = "pjminne"
-    school_email = "pjminne@iu.edu"
+    personal_user = os.getenv("PERSONAL_USER")
+    personal_email = os.getenv("PERSONAL_EMAIL")
+    school_user = os.getenv("SCHOOL_USER")
+    school_email = os.getenv("SCHOOL_EMAIL")
 
-    # Check current Git user
+    
+
+
     current_user, current_email = get_current_git_user()
 
     if current_user == personal_user and current_email == personal_email:
-        # Switch to school account
+
         update_git_credentials(school_user, school_email, "school")
     elif current_user == school_user and current_email == school_email:
-        # Switch to personal account
+
         update_git_credentials(personal_user, personal_email, "personal")
     else:
-        # Handle case where no user is set or unknown credentials are found
+
         print("Unrecognized Git user, switching to personal account by default.")
         update_git_credentials(personal_user, personal_email, "personal")
 
